dac4dd12b5d81207a26f01f0c9ac731a2b65b4cc
kostek <ka.koszewski@gmail.com>
2023-09-01 18:58:04 +0200

    up

--- chat.rb
+++ chat.rb
@@ -1,49 +1,61 @@
 require 'socket'
 
 PORT = 2000
+ConnectedClient = Struct.new(:socket, :username)
 
 class Server
   def initialize 
-    @server = TCPServer.open(PORT)    
+    @server = TCPServer.open(PORT)
     @clients = []
   end 
 
   def run
-    loop do             
-      Thread.start(@server.accept) do |client|
-       @clients << client 
-   
-       client.puts 'Welcome to the chat!'
-       client.puts "It's #{Time.now.utc.strftime("%d/%m/%Y %I:%M %p")} and currently there are #{@clients.size - 1} active users"
-       client.puts 'What is your nickname?'
+    loop do            
+      Thread.start(@server.accept) do |client|   
+        client.puts 'Welcome to the chat!'
+        client.puts "It's #{time_utc("%d/%m/%Y %I:%M %p")} and currently there are #{@clients.size} active users"
+        client.puts 'What is your nickname?'
+        nickname = ''
       
-       nickname = client.gets.chomp
-       client.puts "Hello #{nickname}, enjoy the conversation!"
-       
-       begin
-        while message = client.gets.chomp
-          broadcast(nickname, client, message)
+        loop do 
+          nickname = client.gets.chomp
+          break if nickname_valid?(nickname)
+          client.puts 'invalid nickname. Please try again'
+        end 
+
+        client.puts "Hello #{nickname}, enjoy the conversation!"
+        broadcast("SYSTEM: #{nickname} enters the chat")
+        connected_client = ConnectedClient.new(client, nickname)
+        @clients << connected_client
+        
+        begin
+          while message = client.gets.chomp
+            broadcast(connected_client, "#{nickname}: #{message}")
+          end
+        rescue
+          client.close
+          @clients.delete(connected_client)
+          broadcast("SYSTEM: #{nickname} left chat" )
         end
-       rescue
-        client.close
-        @clients.delete(client)
-        broadcast('SYSTEM', nil, "#{nickname} left chat" )
-       end
       end
     end
   end
 
   private
-  def broadcast(nickname, sender, message)
+  def broadcast(sender = nil, message)
     @clients.each do |client|
       unless client == sender 
-        client.puts "#{Time.now.utc.strftime("%I:%M %p")}, #{nickname}: #{message}"
+        client.socket.puts "#{time_utc}> #{message}"
       end 
     end 
   end
 
   def nickname_valid?(nickname)
-    
+    ((3...20).include? nickname.size) && (@clients.none? {|client| client.username == nickname})
+  end 
+
+  def time_utc(format = "%I:%M %p")
+    Time.now.utc.strftime(format)
   end 
 end 
 
