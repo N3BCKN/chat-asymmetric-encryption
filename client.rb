8f8cc237b86c3c5d25437fb107cf199d6889577e
kostek <ka.koszewski@gmail.com>
2023-09-03 20:19:40 +0200

    refactor

--- client.rb
+++ client.rb
@@ -1,51 +1,54 @@
+# frozen_string_literal: true
+
 require 'socket'
 require 'json'
 require 'openssl'
 require 'base64'
-require 'byebug'
 
 PORT = 2000
+HOST = 'localhost'
 
 class Client
   def initialize
-    @socket       = TCPSocket.new('localhost', PORT)
-    @digest_func  = OpenSSL::Digest::SHA256.new
-    @public_key
-  end 
+    @socket       = TCPSocket.new(HOST, PORT)
+    @digest_func  = OpenSSL::Digest.new('SHA256')
+    @public_key   = nil
+  end
 
   def run
     local_typing_thread = Thread.new { local_typing }
-    receive_from_server_thread = Thread.new { receive_from_server}
+    receive_from_server_thread = Thread.new { receive_from_server }
     local_typing_thread.join
     receive_from_server_thread.join
-    
+
     @socket.close
-  end 
+  end
 
   private
+
   def local_typing
     loop do
       message = gets.chomp
-      if @public_key.nil? 
+      if @public_key.nil?
         @socket.puts message
       else
-        encrypted_message = {message: encrypt(message)}.to_json
+        encrypted_message = { message: encrypt(message) }.to_json
         @socket.puts encrypted_message
-      end 
+      end
     end
   end
 
   def receive_from_server
-    while line = @socket.gets
-      
+    while (line = @socket.gets)
+
       if @public_key.nil? && !valid_json?(line)
         puts line
         next
-      end 
-      
+      end
+
       response = JSON.parse(line)
-  
-      if response.has_key? 'public_key'
+
+      if response.key? 'public_key'
         @public_key = OpenSSL::PKey::RSA.new(response['public_key'])
         next
       end
@@ -53,50 +56,24 @@ class Client
       decrypted_message = decrypt(response['message'])
       signature         = Base64.decode64(response['signature'])
 
-      if valid_signature?(signature,decrypted_message)
+      if valid_signature?(signature, decrypted_message)
         puts decrypted_message
       else
-        p 'ERROR: server responded with invalid signature.'
+        raise 'server responded with invalid signature.'
       end
-
-
-
-
-
-      # if valid_json?(line)
-      #   response = JSON.parse(line)
-      #   if response.has_key? 'message'
-          
-      #     decrypted_message = decrypt(response['message'])
-      #     signature         = Base64.decode64(response['signature'])
-
-      #     if valid_signature?(signature,decrypted_message)
-      #       puts decrypted_message
-      #     else
-      #       p 'wrong signature'
-      #     end 
-      #   elsif response.has_key? 'public_key'
-      #     @public_key = OpenSSL::PKey::RSA.new(response['public_key'])
-      #   else
-      #     p response
-      #   end 
-      # else
-      #   puts line
-      # end 
     end
   end
 
-  private
   def valid_json?(json)
     JSON.parse(json)
     true
-  rescue JSON::ParserError, TypeError => e
+  rescue JSON::ParserError, TypeError
     false
   end
 
   def encrypt(message)
     Base64.encode64(@public_key.public_encrypt(message))
-  end 
+  end
 
   def decrypt(message)
     @public_key.public_decrypt(Base64.decode64(message))
@@ -104,7 +81,7 @@ class Client
 
   def valid_signature?(signature, message)
     @public_key.verify(@digest_func, signature, message)
-  end 
+  end
 end
 
 client = Client.new
--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'socket'
-require 'base64' 
+require 'base64'
 require 'openssl'
 require 'json'
 
@@ -9,74 +11,76 @@ KEY_LENGTH = 4096
 ConnectedClient = Struct.new(:socket, :username)
 
 class Server
-  def initialize 
+  def initialize
     @server       = TCPServer.open(PORT)
     @clients      = []
-    @digest_func  = OpenSSL::Digest::SHA256.new
+    @digest_func  = OpenSSL::Digest.new('SHA256')
     @key_pair     = OpenSSL::PKey::RSA.new(KEY_LENGTH)
     @public_key   = OpenSSL::PKey::RSA.new(@key_pair.public_key.to_der)
-  end 
+  end
 
   def run
-    loop do            
-      Thread.start(@server.accept) do |client|   
+    loop do
+      Thread.start(@server.accept) do |client|
         client.puts 'Welcome to the chat!'
-        client.puts "It's #{server_time("%d/%m/%Y %I:%M %p")} and currently there are #{@clients.size} active users"
+        client.puts "It's #{server_time('%d/%m/%Y %I:%M %p')} and currently there are #{@clients.size} active users"
         client.puts 'What is your nickname?'
         nickname = ''
-      
-        loop do 
+
+        loop do
           nickname = client.gets.chomp
           break if nickname_valid?(nickname)
+
           client.puts 'invalid nickname. Please try again'
-        end 
+        end
 
         client.puts "Hello #{nickname}, enjoy the conversation!"
         broadcast("SYSTEM: #{nickname} enters the chat")
 
-        response = {public_key: @public_key}.to_json
+        response = { public_key: @public_key }.to_json
         client.puts response
 
         connected_client = ConnectedClient.new(client, nickname)
         @clients << connected_client
 
         begin
-          while message = client.gets.chomp
-            decrypted_message = decrypt(JSON.parse(message)["message"])
+          while (message = client.gets.chomp)
+            decrypted_message = decrypt(JSON.parse(message)['message'])
             broadcast(connected_client, "#{nickname}: #{decrypted_message}")
           end
-        rescue
+        rescue StandardError
           client.close
           @clients.delete(connected_client)
-          broadcast("SYSTEM: #{nickname} left chat" )
+          broadcast("SYSTEM: #{nickname} left chat")
         end
       end
     end
   end
 
   private
+
   def broadcast(sender = nil, message)
     @clients.each do |client|
-      unless client == sender
-        full_message = "#{server_time}> #{message}"
-             
-        to_broadcast = {
-          message: encrypt(full_message),
-          signature: sign_message(full_message)
-        }.to_json
+      next if client == sender
 
-        client.socket.puts to_broadcast
-      end 
-    end 
+      full_message = "#{server_time}> #{message}"
+
+      to_broadcast = {
+        message: encrypt(full_message),
+        signature: sign_message(full_message)
+      }.to_json
+
+      client.socket.puts to_broadcast
+    end
   end
 
   def nickname_valid?(nickname)
-    ((3...20).include? nickname.size) && (@clients.none? {|client| client.username == nickname})
-  end 
+    ((3...20).include? nickname.size) && (@clients.none? { |client| client.username == nickname })
+  end
 
-  def server_time(format = "%I:%M %p")
+  def server_time(format = '%I:%M %p')
     Time.now.utc.strftime(format)
-  end 
+  end
 
   def encrypt(message)
     Base64.encode64(@key_pair.private_encrypt(message))
@@ -89,8 +93,7 @@ class Server
   def sign_message(message)
     Base64.encode64(@key_pair.sign(@digest_func, message))
   end
-end 
+end
 
-server = Server.new 
+server = Server.new
 server.run
-
